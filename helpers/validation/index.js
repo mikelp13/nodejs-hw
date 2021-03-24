c632eb4453a69ea15874bae9103b6da7b0a818f0
Mykhailo Kovalchuk <mikekovalchuk13@gmail.com>
2021-03-24 22:59:59 +0200

    added validation

---
+++ helpers/validation/index.js
@@ -0,0 +1,21 @@
+const Joi = require("joi");
+
+module.exports.validateUser = (req, res, next) => {
+
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).max(12).required(),
+  });
+
+  const { error } = schema.validate(req.body);
+
+  if (error) {
+    const [{ message }] = error.details;
+    return res.status(400).json({
+      status: 400,
+      message: `Field: ${message.replace(/"/g, "")}`,
+      data: "Bad Request",
+    });
+  }
+  next();
+};
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const ctrlUser = require("../../controllers/userController");
 const guard = require("../../helpers/guard");
+const { validateUser } = require("../../helpers/validation");
 
-
-router.post("/auth/register", ctrlUser.reg);
-router.post("/auth/login", ctrlUser.login);
+router.post("/auth/register", validateUser, ctrlUser.reg);
+router.post("/auth/login", validateUser, ctrlUser.login);
 router.post("/auth/logout", guard, ctrlUser.logout);
 
 module.exports = router;
